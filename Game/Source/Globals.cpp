f1e15b743441414330217a6cf5b6f7b8553823cd
Akage369 <xaviercasado369@gmail.com>
2022-11-24 00:21:19 +0100

    Cleaning some files

--- Game/Source/Globals.cpp
+++ Game/Source/Globals.cpp
@@ -14,7 +14,7 @@ void log(const char file[], int line, const char* format, ...)
 	va_end(ap);
 	sprintf_s(tmp_string2, 4096, "\n%s(%d) : %s", file, line, tmp_string);
 
-	OutputDebugString(tmp_string2);
+	//OutputDebugString(tmp_string2);
 	
 }
 
